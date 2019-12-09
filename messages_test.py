a701e6e85baa055a8498faa18fd8def86f9e590e
Agata Orzechowska <agata9orzechowska@gmail.com>
2019-12-09 20:50:24 +0100

    Test tweet single message

--- messages_test.py
+++ messages_test.py
@@ -4,3 +4,9 @@ from messages import Twitter
 def test_twitter_initialization():
     twitter = Twitter()
     assert twitter
+
+
+def test_tweet_single_message():
+    twitter = Twitter()
+    twitter.tweet('Hello!')
+    assert twitter.tweets == ['Hello!']
