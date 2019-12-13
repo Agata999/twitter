f0f78c714f339cf471019787d81600503b2e7198
Agata Orzechowska <agata9orzechowska@gmail.com>
2019-12-13 20:13:21 +0100

    Add creating instance Twitter fixture

--- messages_test.py
+++ messages_test.py
@@ -3,19 +3,23 @@ import pytest
 from messages import Twitter
 
 
-def test_twitter_initialization():
+@pytest.fixture
+def twitter():
     twitter = Twitter()
+    yield twitter
+    twitter.delete()
+
+
+def test_twitter_initialization(twitter):
     assert twitter
 
 
-def test_tweet_single_message():
-    twitter = Twitter()
+def test_tweet_single_message(twitter):
     twitter.tweet('Hello!')
     assert twitter.tweets == ['Hello!']
 
 
-def test_tweet_long_message():
-    twitter = Twitter()
+def test_tweet_long_message(twitter):
     with pytest.raises(Exception):
         twitter.tweet('Long message'*20)
     assert twitter.tweets == []
@@ -28,6 +32,5 @@ def test_tweet_long_message():
         ("Message with a #hashtag", ["hashtag"]),
         ("Message #with 2 #hashtags", ["with", "hashtags"])
 ))
-def test_tweet_with_hashtag(message, expected):
-    twitter = Twitter()
+def test_tweet_with_hashtag(twitter, message, expected):
     assert twitter.find_hashtags(message) == expected
