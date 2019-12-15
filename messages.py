bc704b19c6c7d0ac5914d49952af112048425662
Agata Orzechowska <agata9orzechowska@gmail.com>
2019-12-15 16:54:14 +0100

    Write, add and save tweets in a file

--- messages.py
+++ messages.py
@@ -7,7 +7,7 @@ class Twitter:
 
     def __init__(self, backend=None):
         self.backend = backend
-        self.tweets = []
+        self._tweets = []
         if self.backend and not os.path.exists(self.backend):
             with open(self.backend, 'w'):
                 pass
@@ -15,10 +15,20 @@ class Twitter:
     def delete(self):
         print('It\'s the end')
 
+    @property
+    def tweets(self):
+        if self.backend and not self._tweets:
+            with open(self.backend) as twitter_file:
+                self._tweets = [line.rstrip('\n') for line in twitter_file]
+        return self._tweets
+
     def tweet(self, message):
         if len(message) > 160:
             raise Exception('Too long message')
         self.tweets.append(message)
+        if self.backend:
+            with open(self.backend, 'w') as twitter_file:
+                twitter_file.write('\n'.join(self.tweets))
 
     def find_hashtags(self, message):
         return [hashtag.lower() for hashtag in re.findall(r'#(\w+)', message)]
